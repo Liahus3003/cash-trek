b5bba779cc8546640f9f34358921163235ad2c05
suhail <suhailshariff907@gmail.com>
2023-02-26 20:57:47 +0530

    WIP: Refactoring of Modules

--- src/app/modules/cash-book/cash-book.component.spec.ts
+++ src/app/modules/cash-book/cash-book.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { HeaderComponent } from './header.component';
+import { CashBookComponent } from './cash-book.component';
 
-describe('HeaderComponent', () => {
-  let component: HeaderComponent;
-  let fixture: ComponentFixture<HeaderComponent>;
+describe('CashBookComponent', () => {
+  let component: CashBookComponent;
+  let fixture: ComponentFixture<CashBookComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
+      declarations: [ CashBookComponent ]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(HeaderComponent);
+    fixture = TestBed.createComponent(CashBookComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
---
+++ src/app/modules/cash-book/cash-book.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cash-book',
+  templateUrl: './cash-book.component.html',
+  styleUrls: ['./cash-book.component.less'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class CashBookComponent {
+
+}
