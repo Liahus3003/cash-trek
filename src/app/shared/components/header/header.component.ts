82bdd9cc2dcc7eae27a52a75026c47151ff8416a
suhail <suhailshariff907@gmail.com>
2023-02-26 20:58:10 +0530

    WIP: Refactoring of Modules

---
+++ src/app/shared/components/header/header.component.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  imports: [CommonModule],
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.less'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class HeaderComponent {
+
+}
---
+++ src/app/shared/components/navigation-tab/navigation-tab.component.ts
@@ -0,0 +1,16 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { NAVIGATION } from '../../constants';
+
+@Component({
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  selector: 'app-navigation-tab',
+  templateUrl: './navigation-tab.component.html',
+  styleUrls: ['./navigation-tab.component.less'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class NavigationTabComponent {
+  navigationLinks = NAVIGATION;
+}
